b0e24a9ee7ccc9604b1f8a9e054895583e165571
Leonardo Sales <leonardosalesgalvao@gmail.com>
2025-02-26 11:27:46 -0300

    Changes to ROS paths from camera to SM2

--- Examples/ROS/ORB_SLAM3/src/ros_stereo_inertial.cc
+++ Examples/ROS/ORB_SLAM3/src/ros_stereo_inertial.cc
@@ -138,9 +138,9 @@ int main(int argc, char **argv)
     }
 
   // Maximum delay, 5 seconds
-  ros::Subscriber sub_imu = n.subscribe("/imu", 1000, &ImuGrabber::GrabImu, &imugb); 
-  ros::Subscriber sub_img_left = n.subscribe("/camera/left/image_raw", 100, &ImageGrabber::GrabImageLeft,&igb);
-  ros::Subscriber sub_img_right = n.subscribe("/camera/right/image_raw", 100, &ImageGrabber::GrabImageRight,&igb);
+  ros::Subscriber sub_imu = n.subscribe("/imu/data_raw", 1000, &ImuGrabber::GrabImu, &imugb); 
+  ros::Subscriber sub_img_left = n.subscribe("/SM2/left/image_raw", 100, &ImageGrabber::GrabImageLeft,&igb);
+  ros::Subscriber sub_img_right = n.subscribe("/SM2/right/image_raw", 100, &ImageGrabber::GrabImageRight,&igb);
 
   std::thread sync_thread(&ImageGrabber::SyncWithImu,&igb);
 
